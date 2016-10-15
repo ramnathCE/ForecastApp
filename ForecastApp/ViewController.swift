917a44fd1353675c36fa95615af392d5f3de9663
Ramnath Suthakar <ramnath.suthakar@googlemail.com>
2016-10-15 15:01:56 +0100

    Initial Commit

---
+++ ForecastApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ForecastApp
+//
+//  Created by Ramanath Suthakar on 15/10/2016.
+//  Copyright © 2016 Ramnath Suthakar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
